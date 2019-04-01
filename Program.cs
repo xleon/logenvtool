97319fa0aac7302fc59822db8501fe7fb82f0f0c
Diego Ponce de León <malandro@gmail.com>
2019-04-02 00:12:36 +0200

    working tool and readme

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace LogEnvTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogEnv();
+        }
+        
+        private static void LogEnv()
+        {
+            var envVariables = Environment.GetEnvironmentVariables();
+            var dict = envVariables.Keys
+                .Cast<object>()
+                .ToDictionary(k => k, k => envVariables[k])
+                .OrderBy(x => x.Key);
+            
+            foreach (var (key, value) in dict)
+            {
+                if (value is string stringValue && stringValue.Contains(";"))
+                {
+                    var innerValues = stringValue
+                        .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                    if (innerValues.Length > 1)
+                    {
+                        Console.WriteLine($"{key}");
+
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        
+                        innerValues
+                            .OrderBy(v => v)
+                            .ToList()
+                            .ForEach(x => Console.WriteLine($" -> {x}"));
+                        
+                        Console.ResetColor();
+                        
+                        continue;
+                    }
+                }
+
+                Console.Write($"{key} = ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(value);
+                Console.ResetColor();
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# LogEnv
+
+DotNet global tool to log environment variables ordered by name and break inner values (like path) into multiple lines.
+
+<p align="center">
+  <img src="Screenshot.png">
+</p>
+
+# Install
+
+Make sure you have dotnet core installed at your machine.
+
+```console
+dotnet tool install --global LogEnvTool
+```
+
+# Use
+
+From anywhere in your system:
+
+```console
+logenv
+```
+    
